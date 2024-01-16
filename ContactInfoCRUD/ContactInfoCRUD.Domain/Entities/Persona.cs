07da69d1eb51feda371778d09ad450a8e0027cf1
Heidy27 <heidy_rodriguez27@hotmail.com>
2024-01-16 15:33:24 -0400

    Entidades agregadas a la capa de Dominio

---
+++ ContactInfoCRUD/ContactInfoCRUD.Domain/Entities/Persona.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace ContactInfoCRUD.Domain.Entities
+{
+    public class Persona
+    {
+        public Persona()
+        {
+            Contactos = new List<PersonaContacto>();
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Cedula { get; set; }
+        public List<PersonaContacto> Contactos { get; set; }
+    }
+}
---
+++ ContactInfoCRUD/ContactInfoCRUD.Domain/Entities/PersonaContacto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ContactInfoCRUD.Domain.Entities
+{
+    public class PersonaContacto
+    {
+        public int Id { get; set; }
+        public string Celular { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Dirección { get; set; }
+        public int PersonaId { get; set; }
+        public Persona Persona { get; set; }
+    }
+}
